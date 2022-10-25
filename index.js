5d7235a8091b6e1149e816d2ea3013f2a2cfe4dd
Tanvir1909R <trcr719@gmail.com>
2022-10-25 18:04:04 +0600

    category front back added

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const port = process.env.PORT || 5000
 const app = express();
 app.use(cors())
 
+// const courses1 = require('./data/courses1.json');
+const category = require('./data/category.json');
+const frontEnd = require('./data/frontEnd.json')
+
 app.get('/',(req, res)=>{
     const html = `
         <h1>Welcome to learning platform server</h1>
         <li><a href="/courses">Courses Api</a></li>
     `
     res.send(html)
+})
+
+app.get('/category',(req, res)=>{
+    res.send(category)
+})
+
+app.get('/front-end',(req, res)=>{
+    res.send(frontEnd)
 })
 
 app.listen(port,()=>{
