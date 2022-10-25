b3dfc912e4034235be3b8f5588968e9ca5669dac
Tanvir1909R <trcr719@gmail.com>
2022-10-25 18:37:43 +0600

    server ready

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const port = process.env.PORT || 5000
 const app = express();
 app.use(cors())
 
-// const courses1 = require('./data/courses1.json');
+const courses = require('./data/courses.json');
 const category = require('./data/category.json');
 const frontEnd = require('./data/frontEnd.json');
 const backEnd = require('./data/backEnd.json');
-const App = require('./data/app.json')
+const App = require('./data/app.json');
+const game = require('./data/game.json');
+const fullStack = require('./data/fullStack.json')
 
 app.get('/',(req, res)=>{
     const html = `
@@ -18,20 +20,32 @@ app.get('/',(req, res)=>{
     res.send(html)
 })
 
+app.get('/courses',(req, res)=>{
+    res.send(courses)
+})
+
 app.get('/category',(req, res)=>{
     res.send(category)
 })
 
-app.get('/front-end',(req, res)=>{
+app.get('/courses/front-end',(req, res)=>{
     res.send(frontEnd)
 })
 
-app.get('/back-end', (req, res)=>{
+app.get('/courses/back-end', (req, res)=>{
     res.send(backEnd)
 })
 
-app.get('/app-development',(req, res)=>{
+app.get('/courses/app-development',(req, res)=>{
     res.send(App)
+})
+
+app.get('/courses/game-development',(req, res)=>{
+    res.send(game)
+})
+
+app.get('/courses/full-stack',(req, res)=>{
+    res.send(fullStack)
 })
 
 app.listen(port,()=>{
